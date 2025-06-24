09bfa08757836d970dcbe3eb2417cc7ab5ad2c9f
EDSON PEREIRA BARBOZA GOMES <Edsongamer27@gmail.com>
2025-06-24 19:53:38 -0300

    Update: Addition of a new film

--- src/Services/localStorage.ts
+++ src/Services/localStorage.ts
@@ -1,14 +1,17 @@
 // src/Services/localStorage.ts
-
+import DBZ from "../assets/img/Dragon_BallZ.webp"
+import BL from "../assets/img/Blue-Lock-O-Filme.webp"
+import HA from "../assets/img/homem-aranha-de-volta-ao-lar.webp"
+import NC from "../assets/img/uma-noite-de-crimes.webp"
 import type { CinemaData, Filme, Sessao, Assentos } from "../@Types";
 
 // Dados iniciais com IDs ajustados para melhor compatibilidade em URLs
 const initialCineData: CinemaData = {
     filmes: [{
         id: "filme1", // IDs ajustados (minúsculas, sem espaços)
-        titulo: "Dragão Ball Z",
-        poster: "https://i.pinimg.com/originals/5d/40/00/5d40006801ffd481af5ed46ae05374a9.jpg",
-        descricao: "Um garoto chamado goku luta contra tudo e todos para salvar a sua terra",
+        titulo: "Dragão Ball Z: A batalha do Deuses",
+        poster: `${DBZ}`,
+        descricao: "Prepare-se para o retorno triunfal de Dragon Ball Z aos cinemas, numa aventura que redefine os limites do poder e do universo! \n Após décadas de sono, Bills (Beerus), o temível Deus da Destruição, desperta. Com um poder inimaginável, ele fica intrigado com a notícia de que Freeza foi derrotado por um mortal, o Saiyajin Goku.",
         duracao: "2h 30min",
         sessao: [
             { id: "sessao1_filme1", horario: "18:00", sala: "Sala 1", preco: 30, assentosOcupado: ["A1", "A2", "B4", "B1", "A5"] },
@@ -17,9 +20,9 @@ const initialCineData: CinemaData = {
     },
     {
         id: "filme2", // IDs ajustados
-        titulo: "Blue Lock",
-        poster: "https://i.pinimg.com/originals/50/75/76/5075765b5d009cf4393a6b6fae984511.jpg",
-        descricao: "Um grupo de atacantes tentando ser o melhor atacante do mundo, por isso entram no blue lock",
+        titulo: "Blue Lock: Episodio nagi",
+        poster: `${BL}`,
+        descricao: "Acompanhe a história do gênio do futebol, Seishiro Nagi. Cansado da rotina e buscando algo que o desafie, Nagi é arrastado para o programa Blue Lock, um centro de treinamento intensivo para criar o melhor atacante do mundo. Ao lado de seu parceiro Reo Mikage, Nagi descobre um novo propósito no futebol, enfrentando adversários e seus próprios limites em sua busca para dominar o campo e provar que é o verdadeiro egoísta em campo.",
         duracao: "3h 00min",
         sessao: [
             { id: "sessao1_filme2", horario: "18:00", sala: "Sala 3", preco: 50, assentosOcupado: ["C2", "C3", "A1"] },
@@ -29,12 +32,23 @@ const initialCineData: CinemaData = {
     {
         id: "filme3", // IDs ajustados
         titulo: "Homem Aranha - De Volta ao lar",
-        poster: "https://www.papodecinema.com.br/wp-content/uploads/2017/05/20170703-homem-aranha-de-volta-ao-lar-papo-de-cinema-cartaz.webp",
-        descricao: "Um garoto que foi picado por uma aranha radioativa ganhou seus poderes e agora luta para proteger a terra junto aos vingadores",
+        poster: `${HA}`,
+        descricao: "Após sua incrível experiência com os Vingadores, o jovem Peter Parker retorna à sua vida normal, sob a vigilância de Tony Stark. Ansioso para provar seu valor como herói, Peter enfrenta o temível Abutre, que ameaça o que ele mais preza. Ele precisa aprender a equilibrar sua vida escolar com as responsabilidades de ser o Homem-Aranha, enquanto descobre o verdadeiro significado de ser um super-herói.",
         duracao: "2h 25min",
         sessao: [
             { id: "sessao1_filme3", horario: "15:00", sala: "Sala 3", preco: 20, assentosOcupado: ["C2", "C3", "A1"] },
             { id: "sessao2_filme3", horario: "19:30", sala: "Sala 4", preco: 20.50, assentosOcupado: [] }
+        ]
+    },
+    {
+        id: "filme4", // IDs ajustados
+        titulo: "Uma Noite de Crimes (The Purge)",
+        poster: `${NC}`,
+        descricao: "Num futuro próximo, o governo americano instituiu A Noite do Expurgo: doze horas anuais onde todos os crimes, incluindo assassinato, são legais. A família Sandin, rica e segura em sua casa, acredita estar a salvo. Contudo, quando um estranho busca refúgio, eles são forçados a confrontar a brutalidade do evento. A noite se transforma em um pesadelo de sobrevivência.",
+        duracao: "2 horas",
+        sessao: [
+            { id: "sessao1_filme4", horario: "15:00", sala: "Sala 1", preco: 25, assentosOcupado: ["C2", "C3", "A1","A2","A3"] },
+            { id: "sessao2_filme4", horario: "19:30", sala: "Sala 1", preco: 25, assentosOcupado: [] }
         ]
     },
     ]
